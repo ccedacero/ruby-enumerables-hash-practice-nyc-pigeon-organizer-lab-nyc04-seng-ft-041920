c20967471451d77079ed27b60b8b8dd08e1be113
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-04-24 00:13:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+hash = {}
+data.each do |key, value|
+# puts value 
+value.each do |prop, names|
+  # puts " #{prop} : #{name} "
+# puts prop
+  names.each do |name|
+    # puts name
+    hash[name] ||= {}
+    hash[name][key]
+    hash[name][key] ||= []
+    hash[name][key].push(prop.to_s)
+      end
+    end
+  end
+  hash
+  end
