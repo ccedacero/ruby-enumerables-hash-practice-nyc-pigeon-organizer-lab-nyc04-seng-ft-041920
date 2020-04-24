95246953f798cbdec65cbd740e7966690dd2e830
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-04-24 00:13:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,18 @@
 def nyc_pigeon_organizer(data)
-pigeon_list = {}
-data.each do |key, value| 
-pigeon_list[key]= value
-end
-return pigeon_list
-end
+hash = {}
+data.each do |key, value|
+# puts value 
+value.each do |prop, names|
+  # puts " #{prop} : #{name} "
+# puts prop
+  names.each do |name|
+    # puts name
+    hash[name] ||= {}
+    hash[name][key]
+    hash[name][key] ||= []
+    hash[name][key].push(prop.to_s)
+      end
+    end
+  end
+  hash
+  end
